498f8ac2c821c85a07bee8fc5e62dd19f6843a94
MustaphaAwwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-06-28 18:49:23 -0700

    Create 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz():
+    i = range(1, 101)
+    for c in i:
+        if c % 3 == 0 and c % 5 == 0:
+            print('FizzBuzz')
+        elif c % 3 == 0 and c % 5 != 0:
+            print('Fizz')
+        elif c % 3 != 0 and c % 5 == 0:
+            print('Buzz')
+        elif c % 3 != 0 and c % 5 != 0:
+            print(f'{c}')
