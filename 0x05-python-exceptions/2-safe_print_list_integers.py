10c99bfa725008847ea732bf3851f3e54f257116
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-07-18 22:28:12 +0100

    Create 2-safe_print_list_integers.py

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]))
+        except ValueError:
+            continue
+        print()
+        return i
