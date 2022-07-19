06c66896257da63e4a3f34496128db1353cb7612
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-07-19 05:45:35 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    ans = 0
     try:
-        ans = a / b
+        c = a / b
     except Exception:
-        ans = None
+        c = 'none'
+        return c
     finally:
-        print("Inside result: {}".format(ans))
-    return ans
+        print("Inside result: {}".format(c))
+        return c
