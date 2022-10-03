1ddd7ba68fa2381b3b726950d754e58c8123046f
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-03 23:22:16 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,3 +1,3 @@
 #!/usr/bin/node
-const { argv } = require('process');
-console.log(isNaN(parseInt(argv[2])) ? 'Not a number' : `My number: ${parseInt(argv[2])}`);
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
