264070f99e59a48e37bc4b5d9d9035b19930f79e
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-09 09:56:32 +0100

    don

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """
-filter all states from the database
-with name starting from N
+Lists all states with name starting with
+N (upper N) from the database
 """
 from sys import argv
 import MySQLdb
 
 """
 Connection set up
-using MySQLdb
 """
 if __name__ == '__main__':
-    connect = MySQLdb.connect(
-            host='localhost',
+    connection = MySQLdb.connect(
+            host="localhost",
             port=3306, user=argv[1],
             passwd=argv[2],
             db=argv[3],
-            charset="utf8")
-    cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name IS NOT " +
+            charset="utf8"
+    )
+    db = connection.cursor()
+    db.execute("SELECT * FROM states WHERE name IS NOT " +
                "NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY id ASC;")
-    rows = cur.fetchall()
+    query_rows = db.fetchall()
 
-    for row in rows:
+    for row in query_rows:
         print(row)
 
-    cur.close()
-    connect.close()
+    db.close()
+    connection.close()
