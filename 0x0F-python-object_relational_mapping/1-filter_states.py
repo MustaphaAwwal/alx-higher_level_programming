323b685904b3382c070d5dd80d1e75aaef2260e0
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-09 10:13:27 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,20 +10,20 @@ import MySQLdb
 Connection set up
 """
 if __name__ == '__main__':
-    connect = MySQLdb.connect(
-            host='localhost',
-            port=3306, user=argv[1],
-            passwd=argv[2],
-            db=argv[3],
-            charset="utf8"
-    )
-    cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name IS NOT " +
-               "NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY id ASC;")
-    rows = cur.fetchall()
+	connect = MySQLdb.connect(
+		host='localhost',
+		port=3306, user=argv[1],
+		passwd=argv[2],
+		db=argv[3],
+		charset="utf8"
+	)
+	cur = connect.cursor()
+	cur.execute("SELECT * FROM states WHERE name IS NOT " +
+				"NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY id ASC;")
+	rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+	for row in rows:
+		print(row)
 
-    cur.close()
-    connect.close()
+	cur.close()
+	connect.close()
