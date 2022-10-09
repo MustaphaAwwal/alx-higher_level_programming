8ea6f216bf2586aa2ec3f4277806d2277b744296
Musttapha Awwal <awwalmustapha41@gmail.com>
2022-10-09 01:20:00 -0700

    done

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+import MySQLdb
+if __name__ == '__main__':
+    connect = MySQLdb.connect(
+            host=localhost,
+            port='3306',
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3], charset="utf8")
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states OREDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    connect.close()
