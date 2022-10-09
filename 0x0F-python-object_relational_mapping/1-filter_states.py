d942242a5a7fe411e291db4023bf59db3847ed5e
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-09 09:44:33 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
             db=argv[3],
             charset="utf8")
     cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    cur.execute("SELECT * FROM states WHERE name IS NOT " +
+               "NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY id ASC;")
     rows = cur.fetchall()
 
     for row in rows:
