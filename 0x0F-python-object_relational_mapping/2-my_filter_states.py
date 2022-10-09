39c6dabd2b348cead6affed7a4fed6a8d36a13b5
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-09 09:50:41 +0100

    Create 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Lists all states from from the database
+"""
+from sys import argv
+import MySQLdb
+
+"""
+Connection set up
+using MySQLdb
+"""
+if __name__ == '__main__':
+    connect = MySQLdb.connect(
+            host='localhost',
+            port=3306, user=argv[1],
+            passwd=argv[2],
+            db=argv[3],
+            charset="utf8")
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states WHERE" +
+               " CAST(name AS BINARY) LIKE" +
+               " CAST('{}' AS BINARY) ORDER BY id ASC;".format(argv[4]))
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    connect.close()
