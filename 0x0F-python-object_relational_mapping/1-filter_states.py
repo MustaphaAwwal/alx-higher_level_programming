2157a849bb85ac8af6fa51a456e35bfbe38ca652
Mustapha Awwal <101365610+MustaphaAwwal@users.noreply.github.com>
2022-10-09 10:06:19 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == '__main__':
             charset="utf8")
     cur = connect.cursor()
     cur.execute("SELECT * FROM states WHERE name IS NOT " +
-               "NULL AND LEFT(CAST(name AS BINARY), 1) = 'N' ORDER BY id ASC;")
+               "NULL AND LEFT(CAST(name AS BINARY), 1) = \
+               'N' ORDER BY id ASC;")
     rows = cur.fetchall()
 
     for row in rows:
